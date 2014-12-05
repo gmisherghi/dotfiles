673e6eeb72720023f0db49e0d89e317fe26f1e6f
Ghassan Misherghi <spam@gmw6.com>
2014-12-04 22:43:08 -0800

    Adds screenrc to dotfiles.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [[ ! -d $dotfile_dir ]]; then
 fi
 
 cat << EOF
-Warning: This script will destroy your existing .bashrc, .bash_profile, .bash_logout, and .vimrc!  Do you want to continue?  (type yes to continue.)
+Warning: This script will destroy your existing .bashrc, .bash_profile, .bash_logout, .screenrc, and .vimrc!  Do you want to continue?  (type yes to continue.)
 EOF
 
 echo -n "> "
@@ -20,10 +20,11 @@ fi
 
 echo "Setting up your configuration environment."
 set -x
-rm -f ~/.{bashrc,bash_profile,bash_logout,vimrc}
+rm -f ~/.{bashrc,bash_profile,bash_logout,screenrc,vimrc}
 ln -sf ${dotfile_dir}/bash_profile ~/.bash_profile
 ln -sf ${dotfile_dir}/bashrc ~/.bashrc
 ln -sf ${dotfile_dir}/bash_logout ~/.bash_logout
 [[ -d ~/.bash_logs ]] || mkdir ~/.bash_logs/
+ln -sf ${dotfile_dir}/screenrc ~/.screenrc
 ln -sf ${dotfile_dir}/vimrc ~/.vimrc
 mkdir ~/apps > /dev/null 2>&1
